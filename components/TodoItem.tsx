fccf0eadeb3c9637c763edb163a2da9405ac671d
Raz Ego <razego99@gmail.com>
2025-06-29 11:28:25 +0300

    date format

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -80,18 +80,10 @@ const DateChip = styled(Chip)(() => ({
 
 const formatDate = (dateString: string) => {
   const date = new Date(dateString);
-  const now = new Date();
-  const diffInHours = (now.getTime() - date.getTime()) / (1000 * 60 * 60);
-  
-  if (diffInHours < 1) {
-    return 'Just now';
-  } else if (diffInHours < 24) {
-    return `${Math.floor(diffInHours)}h ago`;
-  } else if (diffInHours < 168) { // 7 days
-    return `${Math.floor(diffInHours / 24)}d ago`;
-  } else {
-    return date.toLocaleDateString();
-  }
+  const d = date.getDate().toString().padStart(2, '0');
+  const m = (date.getMonth() + 1).toString().padStart(2, '0');
+  const y = date.getFullYear();
+  return `${d}/${m}/${y}`;
 };
 
 export const TodoItem: React.FC<TodoItemProps> = ({
